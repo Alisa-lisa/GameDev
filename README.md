6467a67075558e18dbb9164fb9c8422ac3fb53a3
alisa <alisa.dammer@gmail.com>
2020-08-01 16:34:10 +0200

    starting with tetra

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GameDev
 tricks and approaches to the game juice
+
+
+
+## Resources
+### Art:
+* [graphics](!https://opengameart.org/content/arcade-platformer-assets)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use tetra::graphics::{self, Color, Texture};
+use tetra::{Context, ContextBuilder, State};
+
+struct GameState {
+    background_texture: Texture,
+}
+
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+        Ok(())
+    }
+}
+fn main() -> tetra::Result {
+    ContextBuilder::new("JuicyGame", 1280, 720)
+        .quit_on_escape(true)
+        .build()?
+        .run(|_ctx| {
+            let backgroubd = Texture::new(_ctx, "./data/global_sprotesheet.png")?;
+            Ok(GameState {background_texture}))
+        }
 }
