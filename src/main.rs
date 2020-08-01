6ed23988b0cd978c452857d0b12c0ff25a048c38
alisa <alisa.dammer@gmail.com>
2020-08-01 17:17:45 +0200

    add player anmation, make spritesheet background transparent

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use tetra::graphics::{self, Color, Texture};
+use tetra::graphics::{self, Color, Texture, DrawParams, Rectangle};
+use tetra::graphics::animation::Animation;
 use tetra::{Context, ContextBuilder, State};
 use tetra::math::Vec2;
-
+use std::time::Duration;
 
 struct GameState {
     background_texture: Texture,
+    player: Animation,
 }
 
 impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
-        graphics::draw(ctx, &self.background_texture, Vec2::new(0.0, 0.0));
+        graphics::draw(ctx, &self.background_texture, DrawParams::new()
+                       .clip(Rectangle::new(0., 176., 160., 128.))
+                       .scale((5.,5.).into()));
+        graphics::draw(ctx, &self.player, DrawParams::new().scale((3.0, 3.0).into()));
+        self.player.advance(ctx); 
         Ok(())
     }
 }
@@ -18,7 +24,8 @@ impl State for GameState {
 impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
         let background_texture = Texture::new(ctx, "./data/global_spritesheet.png")?;
-        Ok(GameState { background_texture })
+        let player = Animation::new(background_texture.clone(), Rectangle::row(0.0, 13.0, 32.0, 19.0).take(3).collect(), Duration::from_secs_f64(0.1));
+        Ok(GameState { background_texture, player })
     }
 
 }
